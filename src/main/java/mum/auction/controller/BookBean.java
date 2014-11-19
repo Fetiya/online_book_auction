290df6f0865fd2fcc6e12879d59b970fd25b2f09
Fetiya <Fetiya@10.10.14.121>
2014-11-18 20:00:35 -0600

    book persistence included

--- src/main/java/mum/auction/controller/BookBean.java
+++ src/main/java/mum/auction/controller/BookBean.java
@@ -14,36 +14,65 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Named;
-
-import mum.auction.domain.Book;
-import mum.auction.dao.intr.BookDAO;
-
-import mum.auction.dao.intr.*;
 import mum.auction.dao.impl.*;
+import mum.auction.dao.intr.*;
+import mum.auction.dao.intr.BookDAO;
+import mum.auction.domain.Book;
+import mum.auction.domain.BookCategory;
 
 /**
  *
  * @author Komal
  */
-
 @Named("bookBn")
 @SessionScoped
-public class BookBean implements Serializable{
-    
-    private Book book;
+public class BookBean implements Serializable {
+
+    private Book book= new Book();
     private List<Book> books = new ArrayList<Book>();
+    private List<BookCategory> bookCategories = new ArrayList<BookCategory>();
+    private BookCategory category = new BookCategory();
+    
+    private Long selectedCategoryId;
+    
+    private DAOFactory factory = DAOFactory.getFactory();
+
     
     
     
-    private DAOFactory  factory=DAOFactory.getFactory();
-    public Book getBook()
-    {
+    public BookBean() {
+
+        initBookCategories();
+    }
+
+    public Long getSelectedCategoryId() {
+        return selectedCategoryId;
+    }
+
+    public void setSelectedCategoryId(Long selectedCategoryId) {
+        this.selectedCategoryId = selectedCategoryId;
+    }
+
+    
+
+    
+    
+    public Book getBook() {
         return book;
     }
+
     
-    public void setBook(Book book)
-    {
+    
+    public void setBook(Book book) {
         this.book = book;
+    }
+
+    public BookCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(BookCategory category) {
+        this.category = category;
     }
 
     /**
@@ -59,47 +88,77 @@ public class BookBean implements Serializable{
     public void setBooks(List<Book> books) {
         this.books = books;
     }
+
+    public List<BookCategory> getBookCategories() {
+        return bookCategories;
+    }
+
+    public void setBookCategories(List<BookCategory> bookCategories) {
+        this.bookCategories = bookCategories;
+    }
+
     
-    public void addBook()
-    {
-        
+    
+    
+    public String  addBook() {
+
         BookDAO bookDao = factory.getBookDAO();
         // bookDao.addBook(book);
         bookDao.beginTransaction();
         bookDao.save(book);
         bookDao.commitTransaction();
         
-        
+        return "confirmBook.xhtml";
+
     }
-   
+
     public void validateBookTitle(FacesContext fc, UIComponent c, Object value) {
-	   if (((String) value).equals(""))
-	      throw new ValidatorException(
-	         new FacesMessage("Please provide title"));
-   }
-    
+        if (((String) value).equals("")) {
+            throw new ValidatorException(
+                    new FacesMessage("Please provide title"));
+        }
+    }
+
     public void validateBookDescription(FacesContext fc, UIComponent c, Object value) {
-	   if (((String) value).equals(""))
-	      throw new ValidatorException(
-	         new FacesMessage("Please provide description"));
-   }
-    
+        if (((String) value).equals("")) {
+            throw new ValidatorException(
+                    new FacesMessage("Please provide description"));
+        }
+    }
+
     public void validateBookAuthor(FacesContext fc, UIComponent c, Object value) {
-	   if (((String) value).equals(""))
-	      throw new ValidatorException(
-	         new FacesMessage("Please provide author"));
-   }
-    
+        if (((String) value).equals("")) {
+            throw new ValidatorException(
+                    new FacesMessage("Please provide author"));
+        }
+    }
+
     public void validateBookPublisher(FacesContext fc, UIComponent c, Object value) {
-	   if (((String) value).equals(""))
-	      throw new ValidatorException(
-	         new FacesMessage("Please provide publisher"));
-   }
-    
+        if (((String) value).equals("")) {
+            throw new ValidatorException(
+                    new FacesMessage("Please provide publisher"));
+        }
+    }
+
     public void validateBookEdition(FacesContext fc, UIComponent c, Object value) {
-	   if (((String) value).equals(""))
-	      throw new ValidatorException(
-	         new FacesMessage("Please provide edition"));
-   }
-   
+        if (((String) value).equals("")) {
+            throw new ValidatorException(
+                    new FacesMessage("Please provide edition"));
+        }
+    }
+
+    public void initBookCategories() {
+        BookCategoryDAO categoryDao = factory.getBookCategoryDAO();
+
+        categoryDao.beginTransaction();
+
+        bookCategories =(ArrayList<BookCategory>) categoryDao.findAll(0, 10);
+
+        for (BookCategory b : bookCategories) {
+            System.out.println("Bookcategory" + b.getName());
+        }
+        categoryDao.commitTransaction();
+
+    }
+
 }
